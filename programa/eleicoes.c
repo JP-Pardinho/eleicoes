b0f2a8b059a8fcbee9716274e5b61c3f2d008779
JP-Pardinho <jppardinhor@gmail.com>
2024-08-23 16:54:03 -0300

    update das struct e das funções void

--- programa/eleicoes.c
+++ programa/eleicoes.c
@@ -2,6 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct{
+    char nomePrtd[50];
+    char siglaPrtd[10];
+}partidos;
+
+typedef struct{
+    char nomeCandidato[50];
+    int idade;
+    int num_canditados;
+    partidos filiacao;
+}candidato;
+
+typedef struct{
+    char nomeFederacao[50];
+    char siglaFederacao[10];
+}federacao;
+
+void inserirPartido(partidos*P, int pos){
+    printf("Digite o nome do partido: ");
+    scanf("%s", P[pos].nomePrtd);
+    printf("Digite a sigla do partido: ");
+    scanf("%s", P[pos].siglaPrtd);
+}
+
+void cadastrarCandidato(candidato*C, int pos){
+    printf("Pardido Filiado: ");
+    scanf("%s", C[pos].filiacao.nomePrtd);
+    printf("Nome Completo: ");
+    scanf("%s", C[pos].nomeCandidato);
+    printf("Idade: ");
+    scanf("%d", C[pos].idade);
+    printf("Numero de eleitor: ");
+    scanf("%d", C[pos].num_canditados);
+}
 
 
 int main() {
