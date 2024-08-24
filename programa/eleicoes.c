d51fb93dbf35c333475a4f9aea2c808f44611dae
JoaquimMHS <joaquimmhs@gmail.com>
2024-08-24 17:57:17 -0300

    Adicionei a função registraVoto

--- programa/eleicoes.c
+++ programa/eleicoes.c
@@ -32,9 +32,31 @@ void cadastrarCandidato(candidato*C, int pos){
     printf("Nome Completo: ");
     scanf("%s", C[pos].nomeCandidato);
     printf("Idade: ");
-    scanf("%d", C[pos].idade);
+    scanf("%d", &(C[pos].idade));
     printf("Numero de eleitor: ");
-    scanf("%d", C[pos].num_canditados);
+    scanf("%d", &(C[pos].num_canditados));
+}
+
+void registrarFederacao(federacao*F, int pos){
+    printf("Digite o nome da Federacao: ");
+    scanf("%s", F[pos].nomeFederacao);
+    printf("Digite a sigla da Federacao: ");
+    scanf("%s", F[pos].siglaFederacao);
+
+}
+
+
+void registraVoto(candidato*C, int numero, int tam){
+    int i;
+    printf("Digite o número do candidato (5 digitos): ");
+    for(i=0; i<tam; i++){
+        if (C[i].num_canditados == numero){
+            printf("%s", C[i].nomeCandidato);
+        } else{
+            printf("VOTO NULO");
+        }
+    }
+
 }
 
 
