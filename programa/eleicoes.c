169791214be7a49d9f618c0c05f50d349a920ae2
JoaquimMHS <joaquimmhs@gmail.com>
2024-08-24 21:01:01 -0300

    Fiz as alocações de memória e a Função que verifica a existência dos Partidos

--- programa/eleicoes.c
+++ programa/eleicoes.c
@@ -2,10 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+// STRUCTS E ALOCAÇÃO DE MEMÓRIA //
 typedef struct{
     char nomePrtd[50];
     char siglaPrtd[10];
 }partidos;
+
+
+int alocaVetPartidos(int tam){
+    /*Função que realiza a alocação de memória para o ponteiro dos partidos.
+    Será exibida uma mensagem de erro, caso a alocação seja mal sucedida e retorna 
+    o ponteiro *vetPartido*/
+    int *vetPartido = NULL;
+    vetPartido = (int*)maloc(tam*sizeof(int));
+
+    if (vetPartido == NULL){
+        printf("Erro: alocação mal sucedida");
+        exit(1);
+
+    }
+    return *vetPartido;
+}
 
 typedef struct{
     char nomeCandidato[50];
@@ -14,11 +31,43 @@ typedef struct{
     partidos filiacao;
 }candidato;
 
+int alocaVetCandidatos(int tam){
+    /*Função que realiza a alocação de memória para o ponteiro dos candidatos.
+    Será exibida uma mensagem de erro, caso a alocação seja mal sucedida e retorna 
+    o ponteiro *vetCandidato*/
+    int *vetCandidato = NULL;
+    vetCandidato = (int*)maloc(tam*sizeof(int));
+
+    if (vetCandidato == NULL){
+        printf("Erro: alocação mal sucedida");
+        exit(1);
+
+    }
+    return *vetCandidato;
+}
+
 typedef struct{
     char nomeFederacao[50];
     char siglaFederacao[10];
 }federacao;
 
+int alocaVetFederacao(int tam){
+    /*Função que realiza a alocação de memória para o ponteiro dos federacoes.
+    Será exibida uma mensagem de erro, caso a alocação seja mal sucedida e retorna 
+    o ponteiro *vetFederacao*/
+    int *vetFederacao = NULL;
+    vetFederacao = (int*)maloc(tam*sizeof(int));
+
+    if (vetFederacao == NULL){
+        printf("Erro: alocação mal sucedida");
+        exit(1);
+
+    }
+    return *vetFederacao;
+}
+// ------------------------------------------------------------------------------------------------------//
+
+// CADASTROS / REGISTROS //
 void inserirPartido(partidos*P, int pos){
     printf("Digite o nome do partido: ");
     scanf("%s", P[pos].nomePrtd);
@@ -45,6 +94,26 @@ void registrarFederacao(federacao*F, int pos){
 
 }
 
+int verificaExistenciaPartido(partidos*P, int tam, char nome){
+    /* A função verifica se o partido digitado pelo usuário existe. A mesma foi criada
+    com a intencao de verificar se o partido existe, a fim de não registrar outro com o
+    mesmo nome. O mesmo acontece para fazer parte da federacao, visto que o partido só pode
+    participar de uma federaçao.
+    Ela retorna 1 caso o partido exista, caso contrário, retorna 0*/
+
+    int i;
+    for(i=0; i<tam; i++){
+        if(strcmp(P[i].nomePrtd, nome) == 0){
+            return 1; //Partido encontrato
+        } 
+    }
+    return 0; // Partido não encontrato
+}
+
+void participarDaFederacao(federacao*f, partidos*P, char nome){
+    
+    
+}
 
 void registraVoto(candidato*C, int numero, int tam){
     int i;
